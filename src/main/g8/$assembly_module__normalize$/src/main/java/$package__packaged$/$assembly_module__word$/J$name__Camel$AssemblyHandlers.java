e3463e9b9654b7d7697f19c7d55243508a6cf419
Pritam Kadam <phkadam2008@gmail.com>
2018-03-20 16:43:31 +0530

    Pritam|Dolly: Add java templates

---
+++ src/main/g8/$assembly_module__normalize$/src/main/java/$package__packaged$/$assembly_module__word$/J$name__Camel$AssemblyHandlers.java
@@ -0,0 +1,93 @@
+package $package$.$assembly_module;format="word"$;
+
+import akka.actor.typed.javadsl.ActorContext;
+import csw.framework.javadsl.JComponentHandlers;
+import csw.framework.scaladsl.CurrentStatePublisher;
+import csw.messages.commands.CommandResponse;
+import csw.messages.commands.ControlCommand;
+import csw.messages.framework.ComponentInfo;
+import csw.messages.location.TrackingEvent;
+import csw.messages.scaladsl.TopLevelActorMessage;
+import csw.services.command.scaladsl.CommandResponseManager;
+import csw.services.location.javadsl.ILocationService;
+import csw.services.logging.javadsl.ILogger;
+import csw.services.logging.javadsl.JLoggerFactory;
+
+import java.util.concurrent.CompletableFuture;
+
+// Assembly specific logic should be written in below handlers.
+// This handlers gets invoked when component receives messages/commands from other component/entity.
+// For example, if one component sends Submit(Setup(args)) command to $name;format="Camel"$Hcd,
+// This will be first validated in the supervisor and then forwarded to Component TLA which first invokes validateCommand hook
+// and if validation is successful, then onSubmit hook gets invoked.
+// You can find more information on this here : https://tmtsoftware.github.io/csw-prod/framework.html
+public class J$name;format="Camel"$AssemblyHandlers extends JComponentHandlers {
+
+    private ILogger log;
+    private CommandResponseManager commandResponseManager;
+    private CurrentStatePublisher currentStatePublisher;
+    private ActorContext<TopLevelActorMessage> actorContext;
+    private ILocationService locationService;
+    private ComponentInfo componentInfo;
+
+    J$name;format="Camel"$AssemblyHandlers(
+            ActorContext<TopLevelActorMessage> ctx,
+            ComponentInfo componentInfo,
+            CommandResponseManager commandResponseManager,
+            CurrentStatePublisher currentStatePublisher,
+            ILocationService locationService,
+            JLoggerFactory loggerFactory
+    ) {
+        super(ctx, componentInfo, commandResponseManager, currentStatePublisher, locationService, loggerFactory);
+        this.currentStatePublisher = currentStatePublisher;
+        this.log = loggerFactory.getLogger(getClass());
+        this.commandResponseManager = commandResponseManager;
+        this.actorContext = ctx;
+        this.locationService = locationService;
+        this.componentInfo = componentInfo;
+    }
+
+    @Override
+    public CompletableFuture<Void> jInitialize() {
+        return CompletableFuture.runAsync(() -> {
+
+        });
+    }
+
+    @Override
+    public CompletableFuture<Void> jOnShutdown() {
+        return CompletableFuture.runAsync(() -> {
+
+        });
+    }
+
+    @Override
+    public void onLocationTrackingEvent(TrackingEvent trackingEvent) {
+
+    }
+
+    @Override
+    public CommandResponse validateCommand(ControlCommand controlCommand) {
+        return null;
+    }
+
+    @Override
+    public void onSubmit(ControlCommand controlCommand) {
+
+    }
+
+    @Override
+    public void onOneway(ControlCommand controlCommand) {
+
+    }
+
+    @Override
+    public void onGoOffline() {
+
+    }
+
+    @Override
+    public void onGoOnline() {
+
+    }
+}
---
+++ src/main/g8/$hcd_module__normalize$/src/main/java/$package__packaged$/$hcd_module__word$/J$name__Camel$HcdBehaviorFactory.java
@@ -0,0 +1,27 @@
+package $package$.$hcd_module;format="word"$;
+
+import akka.actor.typed.javadsl.ActorContext;
+import csw.framework.javadsl.JComponentBehaviorFactory;
+import csw.framework.javadsl.JComponentHandlers;
+import csw.framework.scaladsl.CurrentStatePublisher;
+import csw.messages.scaladsl.TopLevelActorMessage;
+import csw.messages.framework.ComponentInfo;
+import csw.services.command.scaladsl.CommandResponseManager;
+import csw.services.location.javadsl.ILocationService;
+import csw.services.logging.javadsl.JLoggerFactory;
+
+public class J$name;format="Camel"$HcdBehaviorFactory extends JComponentBehaviorFactory {
+
+    @Override
+    public JComponentHandlers jHandlers(
+            ActorContext<TopLevelActorMessage> ctx,
+            ComponentInfo componentInfo,
+            CommandResponseManager commandResponseManager,
+            CurrentStatePublisher currentStatePublisher,
+            ILocationService locationService,
+            JLoggerFactory loggerFactory
+    ) {
+        return new J$name;format="Camel"$HcdHandlers(ctx, componentInfo, commandResponseManager, currentStatePublisher, locationService, loggerFactory);
+    }
+
+}
