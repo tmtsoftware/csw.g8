86836c1aa0865414e5f904922c21d01b25dc82e8
Pritam Kadam <phkadam2008@gmail.com>
2018-03-23 12:10:37 +0530

    Pritam | Dolly : Add subsystem name as a command line param and dynamically generate package name

--- src/main/g8/$hcd_module__normalize$/src/main/java/$package__packaged$/$hcd_module__word$/J$name__Camel$HcdHandlers.java
+++ src/main/g8/$hcd_module__normalize$/src/main/java/$package__packaged$/$hcd_module__word$/J$name__Camel$HcdHandlers.java
@@ -15,12 +15,14 @@ import csw.services.logging.javadsl.JLoggerFactory;
 
 import java.util.concurrent.CompletableFuture;
 
-// Assembly specific logic should be written in below handlers.
-// This handlers gets invoked when component receives messages/commands from other component/entity.
-// For example, if one component sends Submit(Setup(args)) command to $name;format="Camel"$Hcd,
-// This will be first validated in the supervisor and then forwarded to Component TLA which first invokes validateCommand hook
-// and if validation is successful, then onSubmit hook gets invoked.
-// You can find more information on this here : https://tmtsoftware.github.io/csw-prod/framework.html
+/**
+ * Domain specific logic should be written in below handlers.
+ * This handlers gets invoked when component receives messages/commands from other component/entity.
+ * For example, if one component sends Submit(Setup(args)) command to $name;format="Camel"$Hcd,
+ * This will be first validated in the supervisor and then forwarded to Component TLA which first invokes validateCommand hook
+ * and if validation is successful, then onSubmit hook gets invoked.
+ * You can find more information on this here : https://tmtsoftware.github.io/csw-prod/framework.html
+ */
 public class J$name;format="Camel"$HcdHandlers extends JComponentHandlers {
 
     private ILogger log;
