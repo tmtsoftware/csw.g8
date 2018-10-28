2b62601d9fb9f04b88588e26d278eeb9e3e9a7fc
Pritam Kadam <kpritam@thoughtworks.com>
2018-10-28 10:55:01 +0530

    Fix component handlers compilation.

--- src/main/g8/$assembly_module__normalize$/src/main/scala/$package__packaged$/$assembly_module__word$/$name__Camel$AssemblyHandlers.scala
+++ src/main/g8/$assembly_module__normalize$/src/main/scala/$package__packaged$/$assembly_module__word$/$name__Camel$AssemblyHandlers.scala
@@ -2,11 +2,12 @@
 package $package$.$assembly_module;format="word"$
 
 import akka.actor.typed.scaladsl.ActorContext
+import csw.command.client.messages.TopLevelActorMessage
 import csw.framework.models.CswContext
 import csw.framework.scaladsl.ComponentHandlers
-import csw.command.client.messages.TopLevelActorMessage
-import csw.params.commands.{CommandResponse, ControlCommand}
 import csw.location.api.models.TrackingEvent
+import csw.params.commands.CommandResponse._
+import csw.params.commands.ControlCommand
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
@@ -21,31 +22,22 @@ import scala.concurrent.{ExecutionContextExecutor, Future}
 class $name;format="Camel"$AssemblyHandlers(ctx: ActorContext[TopLevelActorMessage], cswCtx: CswContext) extends ComponentHandlers(ctx,cswCtx) {
 
   implicit val ec: ExecutionContextExecutor = ctx.executionContext
-  private val log = cswCtx.loggerFactory.getLogger
+  private val log                           = cswCtx.loggerFactory.getLogger
 
-  override def initialize(): Future[Unit] = {
-    Future.unit
-  }
+  override def initialize(): Future[Unit] = { Future.unit }
 
-  override def onLocationTrackingEvent(trackingEvent: TrackingEvent): Unit = {
-  }
+  override def onLocationTrackingEvent(trackingEvent: TrackingEvent): Unit = {}
 
-  override def validateCommand(controlCommand: ControlCommand): CommandResponse = CommandResponse.Completed(controlCommand.runId)
+  override def validateCommand(controlCommand: ControlCommand): ValidateCommandResponse = Accepted(controlCommand.runId)
 
-  override def onSubmit(controlCommand: ControlCommand): Unit = {
-  }
+  override def onSubmit(controlCommand: ControlCommand): SubmitResponse = Completed(controlCommand.runId)
 
-  override def onOneway(controlCommand: ControlCommand): Unit = {
-  }
+  override def onOneway(controlCommand: ControlCommand): Unit = {}
 
-  override def onShutdown(): Future[Unit] = {
-    Future.unit
-  }
+  override def onShutdown(): Future[Unit] = { Future.unit }
 
-  override def onGoOffline(): Unit = {
-  }
+  override def onGoOffline(): Unit = {}
 
-  override def onGoOnline(): Unit = {
-  }
+  override def onGoOnline(): Unit = {}
 
 }
