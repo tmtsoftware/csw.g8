7740e7f71484c6de4021e4e5424ee89fbc25a0ec
Pritam Kadam <kpritam@thoughtworks.com>
2019-04-04 10:00:50 +0530

    Fix build.

--- src/main/g8/project/Common.scala
+++ src/main/g8/project/Common.scala
@@ -27,15 +27,6 @@ object Common extends AutoPlugin {
       "-Xfuture"
     ),
     javacOptions in (Compile, doc) ++= Seq("-Xdoclint:none"),
-
-    // This is the placeholder for setting JVM options via sbt native packager.
-    // You can add more JVM options below.
-    javaOptions in Universal ++= Seq(
-      //   -J params will be added as jvm parameters
-      "-J-Xmx8GB",
-      "J-XX:+UseG1GC", // G1GC is default in jdk9 and above
-      "J-XX:MaxGCPauseMillis=30" // Sets a target for the maximum GC pause time. This is a soft goal, and the JVM will make its best effort to achieve it
-    ),
     testOptions in Test ++= Seq(
       // show full stack traces and test case durations
       Tests.Argument("-oDF"),
