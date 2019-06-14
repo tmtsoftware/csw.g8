eee74bf542bec61dd6a6f626234894bdac7a2134
Dollyg <dollyg@thoughtworks.com>
2019-06-14 17:17:08 +0530

    #000 : Pritam | Dolly : Replace jitpack resolver by bintray

--- src/main/g8/project/Common.scala
+++ src/main/g8/project/Common.scala
@@ -34,7 +34,7 @@ object Common extends AutoPlugin {
       // -a Show stack traces and exception class name for AssertionErrors.
       Tests.Argument(TestFrameworks.JUnit, "-v", "-a")
     ),
-    resolvers += "jitpack" at "https://jitpack.io",
+    resolvers += Resolver.bintrayRepo("twtmt", "maven"),
     resolvers += "bintray" at "http://jcenter.bintray.com",
     version := "$version$",
     fork := true,
