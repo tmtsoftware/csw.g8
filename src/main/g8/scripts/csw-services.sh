d09862a2c13f1d5a85d48877dac3a6a4736dfc28
kpritam <kpritam@thoughtworks.com>
2020-02-14 11:26:23 +0530

    #CSW-90: Pritam: Exclude coursier and csw-services.sh from g8

--- src/main/g8/scripts/csw-services.sh
+++ src/main/g8/scripts/csw-services.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="\$(cd "\$(dirname "\${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-ROOT_DIR="\$(dirname "\$SCRIPT_DIR")"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 
 # ================================================ #
 # Read csw.version property from build.properties file
 # Use DEFAULT_CSW_VERSION if build.properties file does not exist or csw.version property does not present
 # ================================================ #
-BUILD_PROPERTIES="\$ROOT_DIR/project/build.properties"
-CSW_VERSION=\$(grep "csw.version" "\$BUILD_PROPERTIES" 2>/dev/null | cut -d'=' -f2)
+BUILD_PROPERTIES="$ROOT_DIR/project/build.properties"
+CSW_VERSION=$(grep "csw.version" "$BUILD_PROPERTIES" 2>/dev/null | cut -d'=' -f2)
 
-if [ -z "\$CSW_VERSION" ]; then
+if [ -z "$CSW_VERSION" ]; then
     CSW_VERSION="master-SNAPSHOT"
 fi
 # ================================================ #
 
 function run_csw_services() {
-    echo "====== CSW Version [\$CSW_VERSION] ====="
-    \$SCRIPT_DIR\coursier launch -r jitpack com.github.tmtsoftware.csw:csw-services_2.13:"\$CSW_VERSION" com.typesafe.akka:akka-http-spray-json_2.13:10.1.11 -- "\$@"
+    echo "====== CSW Version [$CSW_VERSION] ====="
+    "$SCRIPT_DIR"/coursier launch -r jitpack com.github.tmtsoftware.csw:csw-services_2.13:"$CSW_VERSION" com.typesafe.akka:akka-http-spray-json_2.13:10.1.11 -- "$@"
 }
 
 # capture version number and store rest of the arguments to arr variable which then passed to run csw_services.sh
-while (("\$#")); do
-    case "\$1" in
+while (("$#")); do
+    case "$1" in
     -v | --version)
-        CSW_VERSION="\$2"
+        CSW_VERSION="$2"
         shift
         shift
         ;;
     *)
-        arr+=("\$1")
+        arr+=("$1")
         shift
         ;;
     esac
 done
 
-run_csw_services "\${arr[@]}"
+run_csw_services "${arr[@]}"
