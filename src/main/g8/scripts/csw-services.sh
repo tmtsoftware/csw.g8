980b46c126656868c490fee9d7dedfaee592a009
kpritam <kpritam@thoughtworks.com>
2020-02-28 16:55:57 +0530

    Update csw-services.sh script

--- src/main/g8/scripts/csw-services.sh
+++ src/main/g8/scripts/csw-services.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ROOT_DIR="$(dirname "$SCRIPT_DIR")"
+DEFAULT_CSW_VERSION="master-SNAPSHOT"
+SCRIPT_NAME=$0
 
 # ================================================ #
 # Read csw.version property from build.properties file
@@ -11,14 +13,40 @@ BUILD_PROPERTIES="$ROOT_DIR/project/build.properties"
 CSW_VERSION=$(grep "csw.version" "$BUILD_PROPERTIES" 2>/dev/null | cut -d'=' -f2)
 
 if [ -z "$CSW_VERSION" ]; then
-    CSW_VERSION="master-SNAPSHOT"
+    CSW_VERSION=$DEFAULT_CSW_VERSION
 fi
 # ================================================ #
 
+CSW_SERVICES_LIB=com.github.tmtsoftware.csw:csw-services_2.13
+
 function run_csw_services() {
     echo "====== CSW Version [$CSW_VERSION] ====="
-    "$SCRIPT_DIR"/coursier launch -r jitpack com.github.tmtsoftware.csw:csw-services_2.13:"$CSW_VERSION" com.typesafe.akka:akka-http-spray-json_2.13:10.1.11 -- "$@"
+    "$SCRIPT_DIR"/coursier launch -r jitpack $CSW_SERVICES_LIB:"$CSW_VERSION" com.typesafe.akka:akka-http-spray-json_2.13:10.1.11 -- "$@"
 }
+
+function usage() {
+    echo
+    echo -e "usage: $SCRIPT_NAME COMMAND [--version | -v <CSW_VERSION>]"
+
+    echo "Options:"
+    echo "  --version | -v <CSW_VERSION> Optional CSW version number used to start services"
+    echo
+    echo "Commands:"
+    echo "  start      Starts all csw services if no options provided"
+    echo "  stop       Stops all csw services, use this only when script is started in the background"
+    echo
+    echo "Note: for more information, run $SCRIPT_NAME start --help"
+
+    exit 0
+}
+
+if [[ $1 == "stop" ]]; then
+    jps -m | grep $CSW_SERVICES_LIB | awk '{print $1}' | xargs kill
+    exit 0
+elif [[ $1 == "--help" || $1 == "-h" ]]; then
+    usage
+    exit 0
+fi
 
 # capture version number and store rest of the arguments to arr variable which then passed to run csw_services.sh
 while (("$#")); do
