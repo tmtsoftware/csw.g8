02841816829a5b3b3769f371be246d796564e3de
ManishGowardipe <gowardipemanish95@gmail.com>
2021-01-12 14:22:06 +0530

    #000: Manish : Fix formatting

--- src/main/g8/$subsystem__lower$-$assemblyComponentName__space,norm$/src/main/scala/$subsystem__lower$/$assemblyComponentName__space,norm,word$/$assemblyComponentName__space,Camel$BehaviorFactory.scala
+++ src/main/g8/$subsystem__lower$-$assemblyComponentName__space,norm$/src/main/scala/$subsystem__lower$/$assemblyComponentName__space,norm,word$/$assemblyComponentName__space,Camel$BehaviorFactory.scala
@@ -4,7 +4,6 @@ import csw.framework.models.CswContext
 import csw.framework.scaladsl.{ComponentBehaviorFactory, ComponentHandlers}
 import csw.command.client.messages.TopLevelActorMessage
 
-
 class $assemblyComponentName;format="space,Camel"$BehaviorFactory extends ComponentBehaviorFactory {
 
   override def handlers(ctx: ActorContext[TopLevelActorMessage], cswCtx: CswContext): ComponentHandlers =
