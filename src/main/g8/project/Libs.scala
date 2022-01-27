6d5f660d96548ad212e7b623970792751a00fd72
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.githhub.com>
2022-01-27 14:24:26 +0530

    #CSW-000: Himanshu | Manish : upgrade deps before release

--- src/main/g8/project/Libs.scala
+++ src/main/g8/project/Libs.scala
@@ -6,11 +6,11 @@ import sbt._
 import scala.util.control.NonFatal
 
 object Libs {
-  val ScalaVersion = "2.13.6"
+  val ScalaVersion = "2.13.8"
 
-  val `scalatest`   = "org.scalatest"          %% "scalatest"   % "3.2.9"    //Apache License 2.0
-  val `scala-async` = "org.scala-lang.modules" %% "scala-async" % "1.0.0-M1" //BSD 3-clause "New" or "Revised" License
-  val `junit-4-13`  = "org.scalatestplus"      %% "junit-4-13"  % "3.2.9.0"
+  val `scalatest`   = "org.scalatest"          %% "scalatest"   % "3.2.11"    //Apache License 2.0
+  val `scala-async` = "org.scala-lang.modules" %% "scala-async" % "1.0.1" //BSD 3-clause "New" or "Revised" License
+  val `junit-4-13`  = "org.scalatestplus"      %% "junit-4-13"  % "3.2.10.0"
   val `mockito`     = "org.scalatestplus"      %% "mockito-3-4" % "3.2.10.0"
 }
 
